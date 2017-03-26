78f5c7aca012c7b3f576896b8b593b62bb25912d
Kimwan Ogot <kimwanogot@Kimwans-MacBook-Pro.local>
2017-03-26 15:16:47 +0800

    updated demo site

--- web/ToastrDemo.jsx
+++ web/ToastrDemo.jsx
@@ -15,15 +15,43 @@ class ToastrDemo extends Component{
 
     showToast(){
         this.setState({id: this.state.id + 1});
-        Toast("Message " + this.state.id);
+        Toast("Just a toast! " + this.state.id);
     }
 
     render(){
         return (
             <div>
                 <ToastrContainer />
-                <div className="jumbotron">
-                    <button className="btn btn-primary" onClick={this.showToast}>Show Toast</button>
+                <div className="container">
+                    <div className="jumbotron">
+                        <h1>react-toastr-basic</h1>
+                        <p>A Basic Toastr that is easy to install and easy to use</p>
+                        <p><a className="btn btn-primary btn-lg" onClick={this.showToast}>Toast It!</a></p>
+                    </div>
+                    <div>
+                        Main.jsx
+                        <pre>
+                            import React, &#123;Component} from 'react' <br/>
+                            import ToastrContainer, &#123;Toast} from 'react-toastr-basic'<br/>
+                            <br/>
+                            class MyMainApp extends Component&#123; <br/>
+                                &nbsp;&nbsp;&nbsp;constructor()&#123;<br/><br/>
+
+
+                                &nbsp;&nbsp;&nbsp;}<br/><br />
+                                &nbsp;&nbsp;&nbsp;showToast()&#123;<br/>
+                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Toast('toast message');<br/>
+                                &nbsp;&nbsp;&nbsp;}<br/><br />
+
+                                &nbsp;&nbsp;&nbsp;render()&#123;<br/>
+                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;return( <br/>
+                                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;button onClick=&#123;this.showToast}&gt;CLICK TO TOAST&lt;/button&gt;<br/>
+                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;)
+                                <br/>
+                                &nbsp;&nbsp;&nbsp;}<br/>
+                            }
+                        </pre>
+                    </div>
                 </div>
             </div>
         )
